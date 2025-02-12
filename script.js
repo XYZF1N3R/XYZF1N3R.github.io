58b5594f790341382b939f70e4cf255a4debe625
XYZF1ND3R <136310440+XYZF1N3R@users.noreply.github.com>
2025-02-12 16:56:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    showLocation("location-farm"); // Начальная локация - ферма
+});
+
+function showLocation(locationId) {
+    document.querySelectorAll(".location").forEach(loc => loc.classList.remove("active"));
+    document.getElementById(locationId).classList.add("active");
+}
+
+function enterHangar() {
+    showLocation("location-hangar");
+}
+
+function enterFarm() {
+    showLocation("location-farm");
+}
+
+function upgradeLocation(location) {
+    const button = document.querySelector(`button[onclick="upgradeLocation('${location}')"]`);
+    if (button) {
+        button.classList.add("upgrade-effect");
+        setTimeout(() => button.classList.remove("upgrade-effect"), 500);
+        console.log(`Локация ${location} улучшена!`);
+    }
+}
 
